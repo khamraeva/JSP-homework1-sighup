7826eec84ac4136871a312323badbab09b6f50d7
Alina Hulkovska <51539228+khamraeva@users.noreply.github.com>
2020-07-23 13:35:33 +0300

    Updates

---
+++ precache-manifest.3e2f161d799bc12cb93d9c15854d6687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0914481be8787025cd934fa1b7b33407",
+    "url": "/jsp-signup/index.html"
+  },
+  {
+    "revision": "ec3f94b3b55358992321",
+    "url": "/jsp-signup/static/css/main.2894e4f6.chunk.css"
+  },
+  {
+    "revision": "b16cecda8fd4ccc07d4a",
+    "url": "/jsp-signup/static/js/2.8bbaac08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jsp-signup/static/js/2.8bbaac08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3f94b3b55358992321",
+    "url": "/jsp-signup/static/js/main.0967908f.chunk.js"
+  },
+  {
+    "revision": "c58c4a58e0f582632073",
+    "url": "/jsp-signup/static/js/runtime-main.8e660fb0.js"
+  }
+]);
